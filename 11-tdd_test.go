6c6a121a870fd7a52293b8d36c0d3850df0ed915
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-10-01 23:19:03 -0300

    feat: Describe a basic test with a dependecy

---
+++ 11-tdd_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+)
+
+// A dependecy we'll rely on
+type thingDoer interface {
+	say(name string)
+}
+
+// A high-level function that depends on said dependency
+func speakWithDoer(name string, doer thingDoer) {
+	doer.say(name)
+}
+
+// A fake doer, manually built by us
+type fakeDoer struct {
+	numCalls int
+}
+
+// Method attached to our manual fake doer
+func (doer *fakeDoer) say(name string) {
+	doer.numCalls = doer.numCalls + 1
+}
+
+func TestTdd(t *testing.T) {
+	// Doing tdd the classic way, not using any libraries and doing a mock manually
+	t.Run("manual fake thing doer does something", func(t *testing.T) {
+		subject := fakeDoer{
+			numCalls: 0,
+		}
+		speakWithDoer("Heisenberg", &subject)
+		if subject.numCalls == 0 {
+			t.Errorf("Expected at least 1 call but got %v", subject.numCalls)
+		}
+	})
+}
