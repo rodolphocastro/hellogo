31b5d2315e6b4ff4d3cdefbb6ae6096c545ffca6
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-10-01 23:32:06 -0300

    feat: Use testify to assert something

--- 11-tdd_test.go
+++ 11-tdd_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // A dependecy we'll rely on
@@ -34,5 +36,14 @@ func TestTdd(t *testing.T) {
 		if subject.numCalls == 0 {
 			t.Errorf("Expected at least 1 call but got %v", subject.numCalls)
 		}
+	})
+
+	// Doing tdd with an assertion library but a manual mock
+	t.Run("manual fake thing doer does something but is asserted with testify", func(t *testing.T) {
+		subject := fakeDoer{
+			numCalls: 0,
+		}
+		speakWithDoer("Heisenberg", &subject)
+		assert.Greater(t, subject.numCalls, 0, "subject should've been called at least once")
 	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rodolphocastro/golanghello
 
 go 1.16
+
+require github.com/stretchr/testify v1.7.0
