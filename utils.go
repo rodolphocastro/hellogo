1b567cfc3179f9a410f614a6ded6afbf6b727d1e
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-06-17 23:06:01 -0300

    refactor: Make K8s functions configurable

--- utils.go
+++ utils.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-const pathToK8s = "./k8s.yaml"
+const defaultPathToK8s = "./k8s.yaml"
 const ciEnvKey = "CI"
 
 // SkipTestIfCI Skips a test if the current environment is a CI pipeline.
@@ -17,7 +17,11 @@ func SkipTestIfCI(t *testing.T) {
 }
 
 // SpinUpMongoK8s Quick and Dirty way to spin up the deployment - invoking kubectl in the os' console.
-func SpinUpMongoK8s(t *testing.T) {
+func SpinUpMongoK8s(t *testing.T, pathToK8s string) {
+	if pathToK8s == "" {
+		pathToK8s = defaultPathToK8s
+	}
+
 	kubeApply := exec.Command("kubectl", "apply", "-f", pathToK8s)
 	if kubeApply.Run() != nil {
 		t.Error("Error while spinning up MongoDb")
@@ -25,7 +29,11 @@ func SpinUpMongoK8s(t *testing.T) {
 }
 
 // CleanUpMongoK8s Quick and Dirty way to delete the deployment - invoking kubectl in the os' console.
-func CleanUpMongoK8s(t *testing.T) {
+func CleanUpMongoK8s(t *testing.T, pathToK8s string) {
+	if pathToK8s == "" {
+		pathToK8s = defaultPathToK8s
+	}
+	
 	kubeDelete := exec.Command("kubectl", "delete", "-f", pathToK8s)
 	if kubeDelete.Run() != nil {
 		t.Error("Error while cleaning up MongoDb")
