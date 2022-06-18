0021dc33d2616dfb91533fe8ad9231472c9f4eba
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-06-17 22:58:51 -0300

    refactor: Move useful test functions into its own file

--- 13-connecting-to-mongo_test.go
+++ 13-connecting-to-mongo_test.go
@@ -8,40 +8,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"os"
-	"os/exec"
 	"testing"
 )
 
-const pathToK8s = "./k8s.yaml"
-const ciEnvKey = "CI"
 const minikubeIp = "192.168.49.2"
 const mongoDbCredentials = "root:notsafe"
 const databaseName = "integration-tests"
 const collectionName = "awesomeThings"
-
-// Skips a test if the current environment is a CI pipeline.
-func skipTestIfCI(t *testing.T) {
-	if os.Getenv(ciEnvKey) != "" {
-		t.Skip("Skipping this test - we're running in a CI environment")
-	}
-}
-
-// Quick and Dirty way to spin up the deployment - invoking kubectl in the os' console.
-func spinUpMongoK8s(t *testing.T) {
-	kubeApply := exec.Command("kubectl", "apply", "-f", pathToK8s)
-	if kubeApply.Run() != nil {
-		t.Error("Error while spinning up MongoDb")
-	}
-}
-
-// Quick and Dirty way to delete the deployment - invoking kubectl in the os' console.
-func cleanUpMongoK8s(t *testing.T) {
-	kubeDelete := exec.Command("kubectl", "delete", "-f", pathToK8s)
-	if kubeDelete.Run() != nil {
-		t.Error("Error while cleaning up MongoDb")
-	}
-}
 
 // Creates a mongodb client for the integration environment.
 func createMongoClient(t *testing.T) *mongo.Client {
@@ -54,18 +27,18 @@ func createMongoClient(t *testing.T) *mongo.Client {
 
 // Attempt to create and tear down a k8s deployment.
 func TestMongoSetup(t *testing.T) {
-	skipTestIfCI(t)
+	SkipTestIfCI(t)
 
-	spinUpMongoK8s(t)
+	SpinUpMongoK8s(t)
 
-	cleanUpMongoK8s(t)
+	CleanUpMongoK8s(t)
 }
 
 // Attempt to connect to a mongodb instance
 func TestMongoClient(t *testing.T) {
-	skipTestIfCI(t)
+	SkipTestIfCI(t)
 
-	spinUpMongoK8s(t)
+	SpinUpMongoK8s(t)
 
 	client := createMongoClient(t)
 	// Pinging the database to confirm we have a connection!
@@ -74,19 +47,19 @@ func TestMongoClient(t *testing.T) {
 		t.Errorf("Something went wrote while pinging: %v", err)
 	}
 
-	cleanUpMongoK8s(nil)
+	CleanUpMongoK8s(nil)
 }
 
 // Access (or create) a Collection in the database
 func TestAccessACollection(t *testing.T) {
 	// Arrange
-	skipTestIfCI(t)
-	spinUpMongoK8s(t)
+	SkipTestIfCI(t)
+	SpinUpMongoK8s(t)
 	client := createMongoClient(t)
 
 	// Act
 	collection := client.Database(databaseName).Collection(collectionName)
-	cleanUpMongoK8s(t)
+	CleanUpMongoK8s(t)
 
 	// Assert
 	if collection == nil {
@@ -105,8 +78,8 @@ type Book struct {
 // Creating and deleting a document within a MongoDB Collection
 func TestInsertAndDeleteDocument(t *testing.T) {
 	// Arrange
-	skipTestIfCI(t)
-	spinUpMongoK8s(t)
+	SkipTestIfCI(t)
+	SpinUpMongoK8s(t)
 	anEntity := Book{
 		Title:  "At the Mountains of Madness",
 		Author: "H.P. Lovecraft",
@@ -126,7 +99,7 @@ func TestInsertAndDeleteDocument(t *testing.T) {
 		t.Errorf("Expected no errors but found: %v", err)
 	}
 
-	cleanUpMongoK8s(t)
+	CleanUpMongoK8s(t)
 
 	// Assert
 	if primitive.ObjectID.IsZero(anEntity.ID) {
