3eef28ac20e02331e20fe9ff1449623a2485d28c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-06-23 22:57:25 -0300

    refactor: Extract file reading logic

--- 14-parsing-gpx-files_test.go
+++ 14-parsing-gpx-files_test.go
@@ -18,20 +18,28 @@ func TestReadFile(t *testing.T) {
 
 	// Act
 	got, err := gpx.ParseBytes(fileBytes)
-	secondGot, secondErr := gpx.ParseFile(pathToGuineaPig)
+	secondGot := readGuineaPigFile(t)
 
 	// Assert
 	if err != nil {
 		t.Errorf("Expected no errors parsing the file, but got: %v", err)
 	}
 
-	if secondErr != nil {
-		t.Errorf("Expected no errors parsing the file, but got: %v", secondErr)
-	}
-
 	if got.Description != secondGot.Description {
 		t.Error("Expected both got to be equal, but they aren't")
 	}
+}
+
+// Read Guinea Pig File
+//
+// This func reads the 14-test-subject.gpx file and returns its data.
+// If an error happens any test using this func fails automatically.
+func readGuineaPigFile(t *testing.T) *gpx.GPX {
+	gpxData, err := gpx.ParseFile(pathToGuineaPig)
+	if err != nil {
+		t.Errorf("Expected no errors, but got: %v", err)
+	}
+	return gpxData
 }
 
 // TODO: https://github.com/tkrajina/gpxgo/blob/321f19554eecf2c5ba914f2bfad70b4458e2819f/gpx/gpx.go#L117 - Iterate until you figure out the total distance.
