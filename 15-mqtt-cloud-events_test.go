c7b36a78091066b8bb6376bd56d8f992f7c3c240
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-06-26 21:06:37 -0300

    chore: Upgrade to golang 1.18

--- 15-mqtt-cloud-events_test.go
+++ 15-mqtt-cloud-events_test.go
@@ -1,13 +1,48 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
+	"testing"
+)
 
 const pathToMQTT = "./environments/development/mqtt.yml"
+const topicName = "my-awesome-topic"
 
-func TestMQTTSetup(t *testing.T) {
+// Gets the MQTT address from Minikube.
+func getMqttAddress() string {
+	return fmt.Sprintf("tcp://%v:32002", minikubeIp)
+}
+
+// Sets up the Environment for these tests.
+func setupTestEnvironment(t *testing.T) {
 	SkipTestIfCI(t)
 
 	SpinUpK8s(t, pathToMQTT)
+}
 
+func TestMQTTSetup(t *testing.T) {
+	setupTestEnvironment(t)
+
+	CleanUpK8s(t, pathToMQTT)
+}
+
+func TestPublishToATopic(t *testing.T) {
+	// Arrange
+	setupTestEnvironment(t)
+
+	// Act
+	options := mqtt.NewClientOptions()
+	options.AddBroker(getMqttAddress())
+	options.SetClientID("go-lang-mqtt-test")
+	client := mqtt.NewClient(options)
+	token := client.Connect()
+	token.Wait()
+	err := token.Error()
+	if err != nil {
+		t.Errorf("Expected no errors but found %v", err)
+	}
+
+	// Assert
 	CleanUpK8s(t, pathToMQTT)
 }
