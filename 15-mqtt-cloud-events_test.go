eb3ea4d8da2749d95a6079dced326603deb59deb
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-06-27 22:10:31 -0300

    test: Publish a message

--- 15-mqtt-cloud-events_test.go
+++ 15-mqtt-cloud-events_test.go
@@ -3,12 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/eclipse/paho.mqtt.golang"
+	"math/rand"
 	"testing"
 	"time"
 )
 
 const pathToMQTT = "./environments/development/mqtt.yml"
 const topicName = "my-awesome-topic"
+const aMessage = "Hello, take me to your leader"
+
+// getRandomMessage gets a random message.
+func getRandomMessage() string {
+	return fmt.Sprintf("%v-%v", aMessage, rand.Int())
+}
+
+// createMqqtClient creates a MQTT client - if an error is found it'll error out the test.
+func createMqqtClient(t *testing.T) mqtt.Client {
+	options := mqtt.NewClientOptions()
+	options.AddBroker(getMqttAddress())
+	options.SetClientID("go-lang-mqtt-test")
+	client := mqtt.NewClient(options)
+	token := client.Connect()
+	token.Wait()
+	err := token.Error()
+	if err != nil {
+		t.Errorf("Expected no errors but found %v", err)
+	}
+
+	return client
+}
 
 // Gets the MQTT address from Minikube.
 func getMqttAddress() string {
@@ -29,22 +52,33 @@ func TestMQTTSetup(t *testing.T) {
 	CleanUpK8s(t, pathToMQTT)
 }
 
-func TestPublishToATopic(t *testing.T) {
+func TestConnectToBroker(t *testing.T) {
 	// Arrange
 	setupTestEnvironment(t)
 
 	// Act
-	options := mqtt.NewClientOptions()
-	options.AddBroker(getMqttAddress())
-	options.SetClientID("go-lang-mqtt-test")
-	client := mqtt.NewClient(options)
-	token := client.Connect()
-	token.Wait()
-	err := token.Error()
+	client := createMqqtClient(t)
+
+	// Assert
+	if client == nil {
+		t.Error("Expected a MQTT Client but found nil")
+	}
+	CleanUpK8s(t, pathToMQTT)
+}
+
+func TestPublishToTopic(t *testing.T) {
+	// Arrange
+	setupTestEnvironment(t)
+	client := createMqqtClient(t)
+
+	// Act
+	publishToken := client.Publish(topicName, 0, true, getRandomMessage())
+	publishToken.Wait()
+	err := publishToken.Error()
+
+	// Assert
 	if err != nil {
 		t.Errorf("Expected no errors but found %v", err)
 	}
-
-	// Assert
 	CleanUpK8s(t, pathToMQTT)
 }
