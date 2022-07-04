fea916a5b67711156b09b1985411b8d09cc3ce43
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-07-03 23:20:47 -0300

    wip: Connect to Immudb

---
+++ 17-immudb_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	immudb "github.com/codenotary/immudb/pkg/client"
+	"testing"
+	"time"
+)
+
+const (
+	pathToImmudb = "./environments/development/immudb.yml"
+)
+
+// getImmudbAddress gets the address for the Immudb Instance
+func getImmudbAddress() string {
+	return getMinikubeIp()
+}
+
+func TestConnectToDatabase(t *testing.T) {
+	// Arrange
+	SkipTestIfCI(t)
+	SpinUpK8s(t, pathToImmudb)
+	time.Sleep(time.Second)
+
+	// Act
+	opts := immudb.DefaultOptions().WithAddress(getImmudbAddress()).WithPort(32003)
+	client := immudb.NewClient().WithOptions(opts)
+	err := client.OpenSession(context.TODO(), []byte("immudb"), []byte("immudb"), "defaultDb")
+
+	// Assert
+	if err != nil {
+		t.Errorf("Expected no errors but found %v", err)
+	}
+
+	//CleanUpK8s(t, pathToImmudb)
+}
