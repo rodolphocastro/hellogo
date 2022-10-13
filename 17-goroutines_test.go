55fdcf4cbf926d9c66a4d7e1803d3616ffee649b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-10-12 23:32:29 -0300

    test: Test out Channels

--- 17-goroutines_test.go
+++ 17-goroutines_test.go
@@ -21,6 +21,7 @@ func doubleSomething(waitTime time.Duration, subject int) int {
 	return subject * 2
 }
 
+// The "go" keyword allows us to spin off goroutines from anywhere in the code.
 func TestTheGoKeyword(t *testing.T) {
 	// Arrange
 	var firstGot = 0
@@ -62,5 +63,45 @@ func TestTheGoKeyword(t *testing.T) {
 	time.Sleep(time.Second)
 	if secondGot != secondExpected {
 		t.Errorf("Expected %d but found %d instead", secondExpected, secondGot)
+	}
+}
+
+// Channels are used to allow goroutines to communicate with one another or with a higher level function
+func TestChannelsAllowGoroutinesToCommunicate(t *testing.T) {
+	// Arrange
+	logger := initializeZap()
+	expected := 8
+	// the 'chan' keyword indicates we want to create a Channel to allow communication to happen
+	results := make(chan int)
+
+	// Act
+	// firing a goroutine that outputs to the results
+	go func() {
+		logger.Info("Starting a goroutine")
+		// the right hand side (rhs) <- operator publishes something to a channel
+		logger.Info("Expected value has been published")
+		results <- doubleSomething(time.Second, expected/2)
+		logger.Info("An unexpected value has been published now")
+		results <- 0
+	}()
+
+	// the left hand side (lhs) <- operator means to read something from the channel
+	// note: both lhs and rhs are *blocking*. Which means that the routine will halt execution until something is read
+	// and the receiving routine will also halt until something is received
+	logger.Info("Waiting for got to be published!")
+	got := <-results
+	logger.Info("Got has been updated", zap.Int("got", got))
+
+	// Assert
+	// since <- blocks the reader and the publisher this should just work
+	if got != expected {
+		t.Errorf("Expected %d but found %d!", expected, got)
+	}
+
+	// now we're asking for a second value!
+	got = <-results
+	logger.Info("Got has been updated", zap.Int("got", got))
+	if got == expected {
+		t.Errorf("Expected anything other than %d, but found %d", expected, expected)
 	}
 }
