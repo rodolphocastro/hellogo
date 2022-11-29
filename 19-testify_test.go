72b355d5011783962620ca72f434a534ffb74c81
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-11-28 23:32:01 -0300

    test: use testify's mocks to stub out and control tests tied to interfaces

--- 19-testify_test.go
+++ 19-testify_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 	"strconv"
 	"testing"
@@ -38,5 +39,73 @@ func TestTestifyAssertions(t *testing.T) {
 			assert.NotSame(expected, got, "the key and its value should be different pointers")
 		})
 		assertionsLogger.Info("done asserting", zap.Int("currentTestKey", expected), zap.Int("currentTestValue", got))
+	}
+}
+
+// FriendlyStructFetcherMocked is a mocked version of FriendlyStructFetcherImpl
+type FriendlyStructFetcherMocked struct {
+	mock.Mock
+}
+
+// fetch is a stub implementation to allow FriendlyStructFetcherMocked to match the FriendlyStructFetcher interface.
+func (receiver *FriendlyStructFetcherMocked) fetch() int {
+	return receiver.Called().Int(0)
+}
+
+// FriendlyStructFetcherImpl is a very real struct that does something. trust me, I'm an engineer.
+type FriendlyStructFetcherImpl struct {
+	returnValue int
+}
+
+// FriendlyStructFetcher defines methods that need to exist in order for a
+// something to be fetched. Nasty stuff.
+type FriendlyStructFetcher interface {
+	// fetch returns an int for something.
+	fetch() int
+}
+
+// fetch returns the return value for the real deal struct!
+func (receiver FriendlyStructFetcherImpl) fetch() int {
+	return receiver.returnValue
+}
+
+// TestTestifyMocks uses the mock object from testify to expedite creating mocks
+// for interfaces and types. this test relies on implementation, mocks and stubs
+// for the FriendlyStructFetcher interface. Those implementations and mocks are
+// the FriendlyStructFetcherImpl and FriendlyStructFetcherMocked structs.
+func TestTestifyMocks(t *testing.T) {
+	// Arrange
+	scenarios := map[int]int{
+		42:   24,
+		55:   55,
+		-10:  10,
+		5555: 1231,
+	}
+	assertionsLogger := testifyLogger.With(zap.Int("totalTestCases", len(scenarios)))
+
+	for input, expected := range scenarios {
+		assertionsLogger.Info("beginning a new scenario", zap.Int("scenarioInput", input), zap.Int("scenarioOutput", expected))
+		t.Run(strconv.Itoa(input), func(t2 *testing.T) {
+			// Arrange
+			assertions := assert.New(t2)
+			var nonMocked FriendlyStructFetcher = FriendlyStructFetcherImpl{returnValue: expected}
+			var mocked = new(FriendlyStructFetcherMocked)
+			mocked.
+				On("fetch").                    // telling that whenever the 'fetch' method is called
+				Run(func(args mock.Arguments) { // we should run a specific closure
+					assertionsLogger.Info("mocking and logging - oh yeah")
+				}).
+				Return(expected) // and finally return something
+
+			// Act
+			gotReal := nonMocked.fetch()
+			gotMocked := mocked.fetch()
+
+			// Assert
+			assertions.Equal(expected, gotReal, "the real implementation should behave as expected")
+			assertions.Equal(expected, gotMocked, "the mocked implementation should also match its settings")
+			assertions.Equal(gotMocked, gotReal, "the real and mocked results should also match")
+		})
+		assertionsLogger.Info("done with the scenario", zap.Int("scenarioInput", input), zap.Int("scenarioOutput", expected))
 	}
 }
