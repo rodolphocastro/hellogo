b197342ff66a0358e74eae75474038e2d5e56235
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2022-12-05 20:44:28 -0300

    test: connect to immudb

--- 21-immudb_test.go
+++ 21-immudb_test.go
@@ -4,33 +4,32 @@ import (
 	"context"
 	immudb "github.com/codenotary/immudb/pkg/client"
 	"testing"
-	"time"
 )
 
 const (
 	pathToImmudb = "./environments/development/immudb.yml"
 )
 
-// getImmudbAddress gets the address for the Immudb Instance
-func getImmudbAddress() string {
-	return getMinikubeIp()
-}
-
 func TestConnectToDatabase(t *testing.T) {
 	// Arrange
 	SkipTestIfMinikubeIsUnavailable(t)
 	SpinUpK8s(t, pathToImmudb)
-	time.Sleep(time.Second)
+	//time.Sleep(time.Second*15)
 
 	// Act
-	opts := immudb.DefaultOptions().WithAddress(getImmudbAddress()).WithPort(32003)
+	opts := immudb.
+		DefaultOptions().
+		WithPort(6666).
+		WithAddress(getMinikubeIp())
+
 	client := immudb.NewClient().WithOptions(opts)
-	err := client.OpenSession(context.TODO(), []byte("immudb"), []byte("immudb"), "defaultDb")
+	err := client.OpenSession(context.TODO(), []byte("immudb"), []byte("immudb"), "defaultdb")
 
 	// Assert
 	if err != nil {
 		t.Errorf("Expected no errors but found %v", err)
 	}
 
-	//CleanUpK8s(t, pathToImmudb)
+	client.CloseSession(context.TODO())
+	CleanUpK8s(t, pathToImmudb)
 }
